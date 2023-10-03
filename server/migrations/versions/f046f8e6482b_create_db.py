4256faf8b9a306ee3906378197773a9048bd7ac6
Saul Ceja <saulceja08@gmail.com>
2023-10-02 22:44:47 -0700

    feat: initialize class database

---
+++ server/migrations/versions/f046f8e6482b_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: f046f8e6482b
+Revises: 
+Create Date: 2023-10-02 22:38:46.624470
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f046f8e6482b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
